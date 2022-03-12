378ff0b8918f5afdcee8c6b1ed8e490140bf894e
Khaled021103 <50780687+delak101@users.noreply.github.com>
2022-03-13 00:41:47 +0200

    Update contacts.cpp

--- study-cases/ContactManager/src/contacts.cpp
+++ study-cases/ContactManager/src/contacts.cpp
@@ -4,6 +4,7 @@ contacts::contacts(int contact_size)
 {
     m_size = contact_size;
     m_users = new user[m_size];
+    m_count = 0;
 }
 
 contacts::~contacts()
@@ -13,24 +14,89 @@ contacts::~contacts()
 
 void contacts::addNewUser()
 {
-    //student to implement
+    user *new_user = new user();
+    new_user->setUserId(m_count+1);
+
+    new_user->readInput();
+
+    m_users[m_count++] = * new_user;
 }
 
-void contacts::delUser()
+void contacts::delUser(int user_id)
 {
-    //student to implement
+    if(m_count==0){
+        cout<< "NO  USER EXIST~!\n";
+        return;
+    }
+    bool deleted=false;
+
+    for(int i=0; i<m_count;++i){
+        if(m_users[i].getUserId()==user_id){
+            //delete from array
+            if(i==m_count-1){
+                m_count--;
+            }else{
+                //replace deleted with last user
+                m_users[i]= m_users[m_count-1];
+                m_count--;
+            }
+            deleted = true;
+            break;
+        }
+    }
+    if(deleted){
+        cout<<"USER DELETED SUCCESSFULLY!\n";
+    }else{
+        cout<< "NO USER EXISTS\n";
+    }
 }
-void contacts::editUser()
+void contacts::editUser(int user_id)
 {
-    //student to implement
+    if(m_count==0){
+        cout<< "NO  USER EXIST~!\n";
+        return;
+    }
+    bool edited=false;
+
+    for(int i=0; i<m_count;++i){
+        if(m_users[i].getUserId()==user_id){
+
+            m_users[i].readInput();
+
+            edited = true;
+            break;
+        }
+    }
+    if(edited){
+        cout<<"USER EDITED SUCCESSFULLY!\n";
+    }else{
+        cout<< "NO USER EXISTS\n";
+    }
 }
 
 void contacts::print()const
 {
-    //student to implement
+    if(m_count==0){
+        cout<< "NO USER FOUND!\n";
+        return;
+    }
+    for(int i=0; i<m_count; ++i){
+        m_users[i].print();
+    }
 }
 
 void contacts::findAll(string key)const
 {
-    //student to implement
+    int found_counter = 0;
+    for(int i = 0 ; i<m_count;++i){
+        if(m_users[i].findAny(key)){
+            m_users[i].print();
+            found_counter++;
+        }
+    }
+    if(found_counter==0){
+        cout<< "NO USER FOUNDED!\n";
+    }else{
+    cout<< found_counter << "FOUND~\n";
+    }
 }
