d3c5895596fcd9931cb010c3568516c3062e2dc1
Khaled021103 <50780687+delak101@users.noreply.github.com>
2022-03-13 00:41:43 +0200

    Update phone.cpp

--- study-cases/ContactManager/src/phone.cpp
+++ study-cases/ContactManager/src/phone.cpp
@@ -1,11 +1,34 @@
 #include "phone.h"
+#include <iostream>
 
-phone::phone(string pho,string type,string desc)
-{
-    //student to implement
+using namespace std;
+
+phone::phone(){
+}
+phone::phone(string phone_number, string phone_type, string phone_description){
+    setPhone(phone_number);
+    setType(phone_type);
+    setDescription(phone_description);
+}
+phone::~phone(){
 }
 
-phone::~phone()
-{
-    //student to implement
+void phone::setPhone(string phone_number){
+    this->m_phone = phone_number;
+}
+
+void phone::setType(string phone_type){
+    this->m_type = phone_type;
+}
+
+void phone::setDescription(string phone_description){
+    this->m_description = phone_description;
+}
+
+bool phone::isMatched(string key) const{
+    return (m_phone.compare(key)==0);
+}
+
+void phone::print() const{
+    cout<< "phone: " << m_phone << "\t |" << m_type << "\t |" << m_description << endl;
 }
