b50d45d2b8b8cd9dca4a4b54c20a8b0de9bea4a3
Khaled021103 <50780687+delak101@users.noreply.github.com>
2022-03-13 00:41:48 +0200

    Update main.cpp

--- study-cases/ContactManager/main.cpp
+++ study-cases/ContactManager/main.cpp
@@ -6,16 +6,16 @@ using namespace std;
 int main()
 {
     int c=-1;
-    contacts contact(10); // contacts size = 10
+    contacts contact(100); // contacts size = 10
 
     while(c!=0){
-        cout <<"1. Print All Contacts\n"
-             <<"2. Add New User\n"
-             <<"3. Search \n"
-             <<"4. Edit Existing User\n"
-             <<"5. Delete User\n"
-             <<"0. Quit\n";
-
+        cout <<"\n1- Print All Contacts\n"
+             <<"2- Add New User\n"
+             <<"3- Search \n"
+             <<"4- Edit Existing User\n"
+             <<"5- Delete User\n"
+             <<"0- Quit\n";
+        cout<<"Enter your choice: ";
         cin >> c;
         switch(c){
         case 1:
@@ -25,18 +25,32 @@ int main()
             contact.addNewUser();
             break;
         case 3:
-            string key;
-            cout << "Enter your keyword to find: ";
-            getline(cin, key);
-            contact.findAll(key);
+            {
+                string key;
+                cout << "Enter your keyword to find: ";
+                cin.ignore();
+                getline(cin, key);
+                contact.findAll(key);
+            }
             break;
-            //student to continue
-            // case 4:
-            //    break;
-            // case 5:
-            //    break:
+        case 4:
+            {
+                int edit_id=0;
+                cout<<"Enter user id to edit: ";
+                cin>>edit_id;
+                contact.editUser(edit_id);
+            }
+            break;
+        case 5:
+            {
+                int del_id=0;
+                cout<<"\nEnter user id to delete: ";
+                cin>>del_id;
+                contact.delUser(del_id);
+            }
+            break;
         }
     }
-    cout << "Thanks!" << endl;
+    cout << "Thanks for using Contact Manager Demo!" << endl;
     return 0;
 }
