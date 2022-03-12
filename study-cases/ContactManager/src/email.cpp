e1b26d86b0cf0b91868f897feb0fe5303abc7727
Khaled021103 <50780687+delak101@users.noreply.github.com>
2022-03-13 00:41:40 +0200

    Update email.cpp

--- study-cases/ContactManager/src/email.cpp
+++ study-cases/ContactManager/src/email.cpp
@@ -1,11 +1,34 @@
 #include "email.h"
+#include <iostream>
 
-email::email(string em,string type,string desc)
-{
-//student to implement
+using namespace std;
+
+email::email(){
+}
+email::email(string email_it, string email_type, string email_desc){
+    setEmail(email_it);
+    setType(email_type);
+    setDescription(email_desc);
+}
+email::~email(){
 }
 
-email::~email()
-{
+void email::setEmail(string email_it){
+   this->m_email = email_it;
+}
 
+void email::setType(string email_type){
+    this->m_type = email_type;
+}
+
+void email::setDescription(string email_desc){
+    this->m_description = email_desc;
+}
+
+bool email::isMatched(string key) const{
+    return (m_email.compare(key)==0);
+}
+
+void email::print() const{
+    cout<< "email: " << m_email << "\t |" << m_type << "\t |" << m_description << endl;
 }
